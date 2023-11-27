ea7320e5fbd031a437e2283b24ac11311ae51e05
BerrieV1 <114003039+BerrieV1@users.noreply.github.com>
2023-11-27 15:56:36 +0100

    Initial commit

---
+++ Assets/Scripts/AnimateHandOnInput.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    public InputActionProperty pinchAction;
+    public InputActionProperty gripAction;
+    public Animator handAnimator;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        float triggerValue = pinchAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+    }
+}
