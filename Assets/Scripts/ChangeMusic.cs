ee300e3a94e19addf96bae7a9000a0cedc1b02ff
PhilippeMazur <philippe.mazur@student.ap.be>
2023-12-13 11:13:10 +0100

    add changes to lift

--- Assets/Scripts/ChangeMusic.cs
+++ Assets/Scripts/ChangeMusic.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class ChangeMusic : MonoBehaviour
 {
-    AudioSource audioSource;
+    private static ChangeMusic _instance;
+    static AudioSource audioSource;
+
+    public static ChangeMusic Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<ChangeMusic>();
+                if (_instance == null)
+                {
+                    Debug.LogError("ChangeMusic script not found on any object in scene.");
+                }
+            }
+            return _instance;
+        }
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = gameObject.GetComponent<AudioSource>();
+        _instance = this;
     }
 
-    public void ChangeAudioClip(AudioClip newClip)
+    public static void ChangeAudioClip(AudioClip newClip)
     {
+        Debug.Log(newClip);
         audioSource.clip = newClip;
+        audioSource.Play();
+
+    }
+    public static void StopMusic()
+    {
+        audioSource.Stop();
     }
 }
--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -9,32 +9,33 @@ public class Elevator : MonoBehaviour
     private Vector3 targetPosition; // The target position of the object
     public GameObject elevatorObject;
     bool trigger = false;
-    private AudioSource musicChanger;
+    private ChangeMusic musicChanger;
     public AudioClip clip;
-    GameObject audioMainLoop;
 
 
     void Start()
     {
         // Set the target position of the object
         targetPosition = new Vector3(elevatorObject.transform.position.x, 1543, elevatorObject.transform.position.z);
-        audioMainLoop = GameObject.Find("Audio_Main_Loop");
-        musicChanger = audioMainLoop.GetComponent<AudioSource>();
     }
 
     void Update()
     {
         if(trigger)
         {
-            if(elevatorObject.transform.position.y < 1543)
+            if(elevatorObject.transform.position.y < 139.5f)
             {
-                elevatorObject.transform.position += new Vector3(0, 0.04f, 0);
+                Debug.Log("elevating");
+                MoveElevatorUp();
+            } else
+            {
+                ChangeMusic.StopMusic();
             }
         }
     }
     private void OnTriggerEnter(Collider other)
     {
-        musicChanger.clip = clip;
+        ChangeMusic.ChangeAudioClip(clip);
         TriggerElevator();
         trigger = true;
     }
@@ -45,5 +46,11 @@ public class Elevator : MonoBehaviour
         // Your logic to trigger the elevator goes here.
         Debug.Log("Elevator button pressed!");
         
+    }
+
+    private void MoveElevatorUp()
+    {
+        // Move the elevator up by 0.01 units per frame
+        elevatorObject.transform.position += new Vector3(0, 90f, 0) * speed * Time.deltaTime;
     }
 }
