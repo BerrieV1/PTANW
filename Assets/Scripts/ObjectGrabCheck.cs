0d68bba4638d96c8c460f8566c779a6991013659
BerrieV1 <114003039+BerrieV1@users.noreply.github.com>
2023-12-20 17:55:11 +0100

    Fix light

--- Assets/Scripts/ObjectGrabCheck.cs
+++ Assets/Scripts/ObjectGrabCheck.cs
@@ -10,41 +10,48 @@ public class ObjectGrabCheck : MonoBehaviour
    public GameObject destroyOnGrab;
    public GameObject appearOnGrab;
    public GameObject lights;
+   public GameObject doorLight;
    public Animator animator1;
     public Animator animator2;
     public Animator animator3;
     public  GameObject followCube;
     public GameObject map;
-    private int timer = 0;
+    private float timer = 0f;
     private float delay = 1f;
-    private float timeElapsedOn;
-    private float timeElapsedOff;
 
    void Start()
    {
-       for (int i = 0; i < lights.transform.childCount; i++)
-       {
-           lights.transform.GetChild(i).GetComponent<Light>().enabled = false;
-       }
-       appearOnGrab.SetActive(false);
+        for (int i = 0; i < lights.transform.childCount; i++)
+        {
+            lights.transform.GetChild(i).GetComponent<Light>().enabled = false;
+        }
+        doorLight.GetComponent<Light>().enabled = false;
+        appearOnGrab.SetActive(false);
    }
 
-   void Update()
-   {
-       //isGrabbed = grabInteractable.isSelected;
-
-       if (isGrabbed)
-       {
+    void Update()
+    {
+        //isGrabbed = grabInteractable.isSelected;
+        if (isGrabbed)
+        {
             appearOnGrab.SetActive(true);
-            for (int i = 0; i < lights.transform.childCount; i++)
+            timer += Time.deltaTime;
+
+            if (timer > delay)
             {
-                lights.transform.GetChild(i).GetComponent<Light>().enabled = true;
+                for (int i = 0; i < lights.transform.childCount; i++)
+                {
+                    bool lightStatus = lights.transform.GetChild(i).GetComponent<Light>().enabled;
+                    lights.transform.GetChild(i).GetComponent<Light>().enabled = !lightStatus;
+                }
+                timer = 0;
             }
+            doorLight.GetComponent<Light>().enabled = true;
             animator1.Play("SinkAnimation");
             animator2.Play("RiseAnimation");
             animator3.Play("AppearingDoorAnimation");
             Destroy(followCube);
             map.SetActive(false);
-       }
-   }
+        }
+    }
 }
